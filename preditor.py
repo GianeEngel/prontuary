2da8fcd6407df2b19d4188fcd3b26a1086c0ad10
GianeEngel <118388626+GianeEngel@users.noreply.github.com>
2022-12-23 11:33:00 -0300

    Update preditor.py

--- preditor.py
+++ preditor.py
@@ -16,10 +16,15 @@ gravidade = ['Low','Medium','High']
 nome = 'giane.sav'
 modelo = joblib.load(nome)
 st.title('AI-Blood - COVID-19 Prognosis')
+siri = 0
+aisi = 0
+nlr = 0
+plr = 0
+sii = 0
 rdw = st.number_input('RDW (%)',min_value=(10),max_value=(9000))
 leu = st.number_input('Leukocytes (10^9/L)',min_value=(0),max_value=(270))
-lin = st.number_input('Lymphocytes (10^9/L)',min_value=(1))#
-mon = st.number_input('Monocytes (10^9/L)',min_value=(1))#
+lin = st.number_input('Lymphocytes (10^9/L)')
+mon = st.number_input('Monocytes (10^9/L)')
 neu = st.number_input('Neutrophils (10^9/L)',min_value=(1),max_value=(35))
 pcr = st.number_input('CRP (mg/dL)')
 pla = st.number_input('Platelets (10^9/L)')#
@@ -34,16 +39,30 @@ if dbm2=='No':
 else:
     dm2 = 1
 none = st.selectbox('Hypertension (HAS):',('No','Yes'))
+
+if lin == 0:
+     nlr = 0
+     plr = 0
+     aisi = 0
+  elif mon == 0:
+      siri = 0
+  else:
+      siri = nlr/float(mon)
+      aisi = (float(neu)*int(pla)*float(mon))/float(lin)
+      nlr = float(neu)/float(lin)
+      plr = float(pla)/float(lin)
+      sii = plr/float(neu)
+
 #nlr = st.number_input('NLR.1')
-nlr = int(neu)/int(lin)
+##nlr = int(neu)/int(lin)
 #plr = st.number_input('PLR.1')
-plr = int(pla)/int(lin)
+##plr = int(pla)/int(lin)
 #sii = st.number_input('SII.1')
-sii = plr/int(neu)
+##sii = plr/int(neu)
 #siri = st.number_input('SIRI.1')
-siri = nlr/int(mon)
+##siri = nlr/int(mon)
 #aisi = st.number_input('AISI.1')
-aisi = (int(neu)*int(pla)*int(mon))/int(lin)
+##aisi = (int(neu)*int(pla)*int(mon))/int(lin)
 
 dt = {'NLR':[nlr],'PLR':[nlr],'SII':[sii],'SIRI':[siri],'AISI':[aisi]}
 formulas = pd.DataFrame(data=dt)
